6b39a4b53dac0dae2ee832b0185812e52d9a8f43
Nancy <ng.toledo@hotmail.com>
2019-08-22 10:28:44 -0500

    Installing styled library. Dynamic rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,47 @@
 'use strict';
 
+//
+
+var templateInfo = {
+    name: 'Indecision APP',
+    description: 'Put your life in the hands of a computer',
+    options: ['Option One', 'Option Two']
+};
+
 var template = React.createElement(
-  'h2',
-  null,
-  'Indecision APP'
+    'div',
+    null,
+    React.createElement(
+        'h2',
+        null,
+        templateInfo.name
+    ),
+    templateInfo.description && React.createElement(
+        'p',
+        null,
+        templateInfo.description
+    ),
+    React.createElement(
+        'p',
+        null,
+        templateInfo.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Two'
+        )
+    )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-var template = <h2>Indecision APP</h2>;
-var appRoot = document.getElementById('app');
+//
+
+const templateInfo ={
+    name : 'Indecision APP',
+    description: 'Put your life in the hands of a computer',
+    options: ['Option One', 'Option Two']
+};
+
+let template = (
+    <div>
+        <h2>{templateInfo.name}</h2> 
+        {templateInfo.description && <p>{templateInfo.description}</p>}
+        <p>{templateInfo.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>One</li>
+            <li>Two</li>
+        </ol>
+    </div>);
+
+let appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
