992c8eb829cb578534f3e5a4b3144e64adae8aa3
Nancy <ng.toledo@hotmail.com>
2019-08-22 12:19:38 -0500

    Manual data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,47 +1,89 @@
 'use strict';
 
-//
-
 var templateInfo = {
     name: 'Indecision APP',
     description: 'Put your life in the hands of a computer',
     options: ['Option One', 'Option Two']
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h2',
-        null,
-        templateInfo.name
-    ),
-    templateInfo.description && React.createElement(
-        'p',
-        null,
-        templateInfo.description
-    ),
-    React.createElement(
-        'p',
-        null,
-        templateInfo.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'One'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Two'
-        )
-    )
-);
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h2',
+            null,
+            templateInfo.name
+        ),
+        templateInfo.description && React.createElement(
+            'p',
+            null,
+            templateInfo.description
+        ),
+        React.createElement(
+            'p',
+            null,
+            templateInfo.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'One'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Two'
+            )
+        ),
+        React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h3',
+                null,
+                'Count: ',
+                count
+            ),
+            React.createElement(
+                'button',
+                { onClick: addOne, className: 'button' },
+                '+1'
+            ),
+            React.createElement(
+                'button',
+                { onClick: minusOne, className: 'button' },
+                '-1'
+            ),
+            React.createElement(
+                'button',
+                { onClick: reset, className: 'button' },
+                'RESET'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,22 +1,55 @@
-//
-
 const templateInfo ={
     name : 'Indecision APP',
     description: 'Put your life in the hands of a computer',
     options: ['Option One', 'Option Two']
 };
 
-let template = (
-    <div>
-        <h2>{templateInfo.name}</h2> 
-        {templateInfo.description && <p>{templateInfo.description}</p>}
-        <p>{templateInfo.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>One</li>
-            <li>Two</li>
-        </ol>
-    </div>);
+let count = 0;
+const addOne = () =>{
+    count++;
+    renderCounterApp();
+};
 
-let appRoot = document.getElementById('app');
+const minusOne = () =>{
+    count--;
+    renderCounterApp();
+};
 
-ReactDOM.render(template, appRoot);
+const reset = () =>{
+    count=0;
+    renderCounterApp();
+};
+
+const appRoot = document.getElementById('app');
+
+
+
+const renderCounterApp = () => {
+    const template = (
+        <div>
+            <h2>{templateInfo.name}</h2> 
+            {templateInfo.description && <p>{templateInfo.description}</p>}
+            <p>{templateInfo.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <ol>
+                <li>One</li>
+                <li>Two</li>
+            </ol>
+            <div>
+                <h3>
+                    Count: {count}
+                </h3>
+                <button onClick={addOne} className="button">
+                    +1
+                </button>
+                <button onClick={minusOne} className="button">
+                    -1
+                </button>
+                <button onClick={reset} className="button">
+                    RESET
+                </button>
+            </div>
+        </div>);
+        ReactDOM.render(template, appRoot);
+};
+
+renderCounterApp();
